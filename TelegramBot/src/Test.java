133a47d943cf6265aa492ab120fbc797b7137d8c
SancheeZzz <valera.redis@gmail.com>
2017-09-19 01:19:01 +0300

    В TEST РЕАЛИЗОВАЛ "СОРТИРОВКУ" ПО ДНЯМ

--- TelegramBot/src/Test.java
+++ TelegramBot/src/Test.java
@@ -6,43 +6,89 @@ import java.io.IOException;
 
 public class Test {
 
-//    public String[] getMessage(String cases) throws IOException {
-//        Document doc;
-//        doc = Jsoup.connect("https://www.rozklad.onaft.edu.ua/guest_n.php?view=g&id=195").get();
-//        Elements lessons;
-//        String[] schedule = new String[0];
-//        switch (cases) {
-//            case "/Понедельник":
-//                lessons=doc.getElementsByAttributeValue("title", "Понеділок 1-а пара");
-//                schedule[0]=lessons.text();
-//                break;
-//            case "/Вторник":
-//                lessons=doc.getElementsByAttributeValue("title", "Вівторок 1-а пара");
-//                schedule = lessons.text();
-//                break;
-//            case "/Среда":
-//                lessons=doc.getElementsByAttributeValue("title", "Середа 1-а пара");
-//                schedule = lessons.text();
-//                break;
-//            case "/Четверг":
-//                lessons=doc.getElementsByAttributeValue("title", "Четвер 1-а пара");
-//                schedule = lessons.text();
-//                break;
-//            case "/Пятница":
-//                lessons=doc.getElementsByAttributeValue("title", "П'ятниця 1-а пара");
-//                schedule = lessons.text();
-//                break;
-//            case "/Суббота":
-//                lessons=doc.getElementsByAttributeValue("title", "Субота 1-а пара");
-//                schedule += lessons.text();
-//                break;
-//            default:
-//                System.out.println("Введите день недели, плиз...");
-//
-//
-//        }
-//        return schedule;
-//    }
+    public String[] getMessage(String cases) throws IOException {
+        Document doc;
+        doc = Jsoup.connect("https://www.rozklad.onaft.edu.ua/guest_n.php?view=g&id=195").get();
+        Elements lesson1;
+        Elements lesson2;
+        Elements lesson3;
+        Elements lesson4;
+        String[] schedule = new String[0];
+        switch (cases) {
+
+            case "/Понедельник":
+                lesson1=doc.getElementsByAttributeValue("title", "Понеділок 1-а пара");
+                lesson2=doc.getElementsByAttributeValue("title", "Понеділок 2-а пара");
+                lesson3=doc.getElementsByAttributeValue("title", "Понеділок 3-а пара");
+                lesson4=doc.getElementsByAttributeValue("title", "Понеділок 4-а пара");
+                schedule[0]=lesson1.text();
+                schedule[1]=lesson2.text();
+                schedule[2]=lesson3.text();
+                schedule[3]=lesson4.text();
+                break;
+
+            case "/Вторник":
+                lesson1=doc.getElementsByAttributeValue("title", "Вівторок 1-а пара");
+                lesson2=doc.getElementsByAttributeValue("title", "Вівторок 2-а пара");
+                lesson3=doc.getElementsByAttributeValue("title", "Вівторок 3-а пара");
+                lesson4=doc.getElementsByAttributeValue("title", "Вівторок 4-а пара");
+                schedule[0]=lesson1.text();
+                schedule[1]=lesson2.text();
+                schedule[2]=lesson3.text();
+                schedule[3]=lesson4.text();
+                break;
+
+            case "/Среда":
+                lesson1=doc.getElementsByAttributeValue("title", "Середа 1-а пара");
+                lesson2=doc.getElementsByAttributeValue("title", "Середа 2-а пара");
+                lesson3=doc.getElementsByAttributeValue("title", "Середа 3-а пара");
+                lesson4=doc.getElementsByAttributeValue("title", "Середа 4-а пара");
+                schedule[0]=lesson1.text();
+                schedule[1]=lesson2.text();
+                schedule[2]=lesson3.text();
+                schedule[3]=lesson4.text();
+                break;
+
+            case "/Четверг":
+                lesson1=doc.getElementsByAttributeValue("title", "Четвер 1-а пара");
+                lesson2=doc.getElementsByAttributeValue("title", "Четвер 2-а пара");
+                lesson3=doc.getElementsByAttributeValue("title", "Четвер 3-а пара");
+                lesson4=doc.getElementsByAttributeValue("title", "Четвер 4-а пара");
+                schedule[0]=lesson1.text();
+                schedule[1]=lesson2.text();
+                schedule[2]=lesson3.text();
+                schedule[3]=lesson4.text();
+                break;
+
+            case "/Пятница":
+                lesson1=doc.getElementsByAttributeValue("title", "П`ятниця 1-а пара");
+                lesson2=doc.getElementsByAttributeValue("title", "П`ятниця 2-а пара");
+                lesson3=doc.getElementsByAttributeValue("title", "П`ятниця 3-а пара");
+                lesson4=doc.getElementsByAttributeValue("title", "П`ятниця 4-а пара");
+                schedule[0]=lesson1.text();
+                schedule[1]=lesson2.text();
+                schedule[2]=lesson3.text();
+                schedule[3]=lesson4.text();
+                break;
+
+            case "/Суббота":
+                lesson1=doc.getElementsByAttributeValue("title", "Понеділок 1-а пара");
+                lesson2=doc.getElementsByAttributeValue("title", "Понеділок 2-а пара");
+                lesson3=doc.getElementsByAttributeValue("title", "Понеділок 3-а пара");
+                lesson4=doc.getElementsByAttributeValue("title", "Понеділок 4-а пара");
+                schedule[0]=lesson1.text();
+                schedule[1]=lesson2.text();
+                schedule[2]=lesson3.text();
+                schedule[3]=lesson4.text();
+                break;
+
+            default:
+                System.out.println("Введите день недели, плиз...");
+
+
+        }
+        return schedule;
+    }
 
 
 
