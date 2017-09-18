6dd801fc4e78be6dd3ec7a928dd9db00a38c0ef2
SancheeZzz <valera.redis@gmail.com>
2017-09-19 01:08:03 +0300

    Bot

--- TelegramBot/src/Test.java
+++ TelegramBot/src/Test.java
@@ -3,26 +3,55 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Test {
+
+//    public String[] getMessage(String cases) throws IOException {
+//        Document doc;
+//        doc = Jsoup.connect("https://www.rozklad.onaft.edu.ua/guest_n.php?view=g&id=195").get();
+//        Elements lessons;
+//        String[] schedule = new String[0];
+//        switch (cases) {
+//            case "/Понедельник":
+//                lessons=doc.getElementsByAttributeValue("title", "Понеділок 1-а пара");
+//                schedule[0]=lessons.text();
+//                break;
+//            case "/Вторник":
+//                lessons=doc.getElementsByAttributeValue("title", "Вівторок 1-а пара");
+//                schedule = lessons.text();
+//                break;
+//            case "/Среда":
+//                lessons=doc.getElementsByAttributeValue("title", "Середа 1-а пара");
+//                schedule = lessons.text();
+//                break;
+//            case "/Четверг":
+//                lessons=doc.getElementsByAttributeValue("title", "Четвер 1-а пара");
+//                schedule = lessons.text();
+//                break;
+//            case "/Пятница":
+//                lessons=doc.getElementsByAttributeValue("title", "П'ятниця 1-а пара");
+//                schedule = lessons.text();
+//                break;
+//            case "/Суббота":
+//                lessons=doc.getElementsByAttributeValue("title", "Субота 1-а пара");
+//                schedule += lessons.text();
+//                break;
+//            default:
+//                System.out.println("Введите день недели, плиз...");
+//
+//
+//        }
+//        return schedule;
+//    }
+
+
 
     public void parseHtml() {
 
         Document doc;
-        String title="";
-        String day = "";
-
-        String subj="";
-        String teacher="";
-        String auditory="";
-        Elements days;
-        Elements teachers;
-        Elements auditories;
-        String [] week=null;
-        String allDay="";
+        Elements days= new Elements();
+        Elements subjects= new Elements();
+        Elements titles = new Elements();
 
 
         //надо брать все элементы из <tbody> пока они есть...
@@ -30,23 +59,16 @@ public class Test {
 
         try {
             doc = Jsoup.connect("https://www.rozklad.onaft.edu.ua/guest_n.php?view=g&id=195").get();
-            days = doc.getElementsByAttributeValue("class","entry-title");
-
-
-
-
-
-            day= days.tagName("h1").first().text();
-
-
-             title= doc.title();
-
+            days = doc.getElementsByAttributeValue("class","lesson");
+//            subjects = doc.getElementsByAttributeValue("class" , "predm");
+//            titles = doc.getElementsByAttributeValue("title", "Понеділок 1-а пара");
 
         } catch (IOException e) {
             e.getMessage();
         }
-        System.out.println("A title is : " + title);
-        System.out.println("A current day is : " + allDay);
+
+        System.out.println("Lessons : " + days.text());
+        System.out.println("Subjects : " + titles.text());
 
     }
 
