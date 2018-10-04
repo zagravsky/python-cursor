bfc03e90d3f092f3524901cc2b5619bb424f61af
Ruslan Karelov <ruslan.karelov@gmail.com>
2018-10-04 11:53:16 +0300

    Fixed bug in task_7.py

--- week2/lesson2-intro-to-functional/homework/RuslanKarelov/utils/task_7.py
+++ week2/lesson2-intro-to-functional/homework/RuslanKarelov/utils/task_7.py
@@ -1,5 +1,5 @@
 def convert_number(arabic_number: int):
-    roman_numb = {1: 'I', 4: 'IV', 5: 'V', 9: 'XI', 10: 'X',
+    roman_numb = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X',
                   40: 'XL', 50: 'L', 90: 'XC', 100: 'C',
                   400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
     your_num = ""
