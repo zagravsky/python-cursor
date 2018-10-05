6701a765190626559b5ea1dc2fcc624bb99743b3
RomanBuryy <roman.buriy@gmail.com>
2018-10-05 10:20:59 +0300

    week_2_lesson_2_roman_buryy corrected errors in Task_5

--- week2/lesson2-intro-to-functional/homework/RomanBuryy/utils/Task_5.py
+++ week2/lesson2-intro-to-functional/homework/RomanBuryy/utils/Task_5.py
@@ -2,9 +2,9 @@ from functools import reduce
 
 
 def summary(members: list):
-    youngest = min(members, key=lambda x: list(x.values())[0])
+    youngest = min(members, key=lambda x: x["age"])
 
-    oldest = max(members, key=lambda x: list(x.values())[0])
+    oldest = max(members, key=lambda x: x["age"])
 
     sum_age = reduce(lambda x, y: x + y, list(i["age"] for i in members))
 
