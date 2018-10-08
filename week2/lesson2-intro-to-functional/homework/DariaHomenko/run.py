7b4b7908621dc56d86f00d0891aea8e49a6ee6c8
Daria_Homenko <darishahomenkogmail.comgit commit -m First>
2018-10-08 20:26:27 +0300

    week2_lesson2_Daria_Homenko Added run file

---
+++ week2/lesson2-intro-to-functional/homework/DariaHomenko/run.py
@@ -0,0 +1,28 @@
+from my_moduls import *
+
+if __name__ == '__main__':
+    members = [
+        {'age': 43, 'name': 'Denis'},
+        {'age': 49, 'name': 'Roman'},
+        {'age': 36, 'name': 'Godzilla'},
+        {'age': 47, 'name': 'Spike'},
+        {'age': 31, 'name': 'SuperMan'},
+        {'age': 49, 'name': 'Batman'},
+        {'age': 37, 'name': 'Claus'},
+        {'age': 55, 'name': 'Frank'},
+        {'age': 83, 'name': 'Homer'}
+    ]
+
+print('--------------Task 1--------------', end='\n\n')
+print(print_list(members), end='\n\n')
+print('--------------Task 2--------------', end='\n\n')
+print(name_uppercase(members), end='\n\n')
+print('--------------Task 3--------------', end='\n\n')
+print(load_func(members), end='\n\n')
+print('--------------Task 4--------------', end='\n\n')
+print(members_with_o(members), end='\n\n')
+print('--------------Task 5--------------', end='\n\n')
+print(sum_max_min_members(members))
+print('--------------Task 6--------------', end='\n\n')
+print(sort_members(members), end='\n\n')
+print('--------------Task 7--------------', end='\n\n')
