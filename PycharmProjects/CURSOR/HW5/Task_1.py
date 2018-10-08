ff5a6c88caf4303df8dcf41507da22f76d8d5948
SancheeZzz <valera.redis@gmail.com>
2018-10-08 16:04:22 +0300

    Homework 5 OS,HTTP

---
+++ PycharmProjects/CURSOR/HW5/Task_1.py
@@ -0,0 +1,16 @@
+import os
+
+def dir_and_file(user_path: str) -> list:
+    tree = []
+    for dir_path, dir_names, file_names in os.walk(user_path):
+        lvl_dir = dir_path.replace(user_path, '').count(os.sep)
+        tab = " " * 4 * lvl_dir
+        tree.append(tab + os.path.basename(dir_path))
+        sub_tab = ' ' * 4 * (lvl_dir + 1)
+        for file in file_names:
+            tree.append(sub_tab + file)
+    return tree
+if __name__ == "__main__":
+    import pprint
+    path = 'C:/Users/SancheeZzz/PycharmProjects/CURSOR/SancheeZzz'
+    pprint.pprint(dir_and_file(path))
