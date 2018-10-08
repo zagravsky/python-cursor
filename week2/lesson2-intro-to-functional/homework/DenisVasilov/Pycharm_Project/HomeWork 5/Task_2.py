7e5e5f83c0b986268bed6cc8f1f2d50f15f3528e
SancheeZzz <valera.redis@gmail.com>
2018-10-08 16:15:20 +0300

    HW5 HTTP+OS

---
+++ week2/lesson2-intro-to-functional/homework/DenisVasilov/Pycharm_Project/HomeWork 5/Task_2.py
@@ -0,0 +1,43 @@
+import csv
+def create_csv (stud_list:list, doc_name:str):
+    with open(doc_name + ".csv", 'w', newline='') as csv_file:
+        file_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for stud in stud_list:
+            file_writer.writerow(stud)
+
+
+# if __name__ == '__main__':
+#     group_list = [['Alexandr', 'Klimach', '@klimach'],
+#                   ['Alexander', 'Kozhokar', '@hey_alex'],
+#                   ['Viktor', 'B.', '@sancheezzz'],
+#                   ['Anton', 'Ivanov', None],
+#                   ['Roman', 'Rodomansky', '@romanrodomansky'],
+#                   ['Pavlo', 'Kandiak', '@kandiak_ps'],
+#                   ['Eugene', 'Zabolotny', '@eugenezabolotny'],
+#                   ['Дмитро', 'Bragarnik', '@Sarbai'],
+#                   ['Eugene', 'Semanyshyn', None],
+#                   ['Anton', 'Dotsenko', None],
+#                   ['Ruslan', 'Karelov', None],
+#                   ['Ostap', 'Rodomansky', '@ostap_rodomansky'],
+#                   ['Igor', 'Tagintsev', '@Ingvar1390'],
+#                   ['Maxim', 'Poleshko', '@Maxsim_P'],
+#                   ['Misha', 'Antonkin', '@Cosmander'],
+#                   ['Андрій', 'Станішевський', '@xlibchyk'],
+#                   ['Olga', 'Tregub', None],
+#                   ['Roman', 'Zhovna', None],
+#                   ['Yevheniia', 'Kyryliuk', '@EvgeniaCURSOR'],
+#                   ['Roman', 'Buryy', None],
+#                   ['Almost wise', 'Dragon', '@AlmostWise'],
+#                   ['Natali', 'Maslova', None],
+#                   ['Andrii', 'Homeniuk', '@AlmostWise'],
+#                   ['Arthur', 'Veres', '@Artooi'],
+#                   ['Dmytro', 'Melnyk', '@Ekut_v'],
+#                   ['Albert', 'Li', None],
+#                   ['Bohdan', 'Novakivskyy', '@AlmostWise'],
+#                   ['Igor', None, '@siriusnlo'],
+#                   ['Anna', None, None],
+#                   ['Igorosha', 'Prorochuk', None],
+#                   ['Jenia', 'Trofimenko', '@jeniaTrofimenko'],
+#                   ['Alina', 'Brygas', None],
+#                   ['Eugen', None, None]]
+#     create_csv(group_list,"Csv_task_2")
