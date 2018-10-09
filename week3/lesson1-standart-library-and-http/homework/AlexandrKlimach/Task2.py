0833f98cc01d9d0a731e6f304110526d704ae0ec
Klimach <klimach95@gmail.com>
2018-10-09 21:47:48 +0300

    correct hw5

--- week3/lesson1-standart-library-and-http/homework/AlexandrKlimach/Task2.py
+++ week3/lesson1-standart-library-and-http/homework/AlexandrKlimach/Task2.py
@@ -2,47 +2,47 @@ import csv
 
 
 def create_csv_file(lst: list, file_name: str):
-    with open(file_name + ".csv", 'w', newline='') as csv_file:
+    with open(file_name + ".csv", 'w') as csv_file:
         columns = ['First name', 'Last name', 'Telegram tag']
-        file_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        file_writer.writerow(column for column in columns)
-        for member in lst:
-            file_writer.writerow(member)
+        file_writer = csv.DictWriter(csv_file, fieldnames=columns)
+        file_writer.writeheader()
+        file_writer.writerows(lst)
 
 
 if __name__ == "__main__":
-    group_list = [['Alexandr', 'Klimach', '@klimach'],
-                  ['Alexander', 'Kozhokar', '@hey_alex'],
-                  ['Viktor', 'B.', '@sancheezzz'],
-                  ['Anton', 'Ivanov', None],
-                  ['Roman', 'Rodomansky', '@romanrodomansky'],
-                  ['Pavlo', 'Kandiak', '@kandiak_ps'],
-                  ['Eugene', 'Zabolotny', '@eugenezabolotny'],
-                  ['Дмитро', 'Bragarnik', '@Sarbai'],
-                  ['Eugene', 'Semanyshyn', None],
-                  ['Anton', 'Dotsenko', None],
-                  ['Ruslan', 'Karelov', None],
-                  ['Ostap', 'Rodomansky', '@ostap_rodomansky'],
-                  ['Igor', 'Tagintsev', '@Ingvar1390'],
-                  ['Maxim', 'Poleshko', '@Maxsim_P'],
-                  ['Misha', 'Antonkin', '@Cosmander'],
-                  ['Андрій', 'Станішевський', '@xlibchyk'],
-                  ['Olga', 'Tregub', None],
-                  ['Roman', 'Zhovna', None],
-                  ['Yevheniia', 'Kyryliuk', '@EvgeniaCURSOR'],
-                  ['Roman', 'Buryy', None],
-                  ['Almost wise', 'Dragon', '@AlmostWise'],
-                  ['Natali', 'Maslova', None],
-                  ['Andrii', 'Homeniuk', '@AlmostWise'],
-                  ['Arthur', 'Veres', '@Artooi'],
-                  ['Dmytro', 'Melnyk', '@Ekut_v'],
-                  ['Albert', 'Li', None],
-                  ['Bohdan', 'Novakivskyy', '@AlmostWise'],
-                  ['Igor', None, '@siriusnlo'],
-                  ['Anna', None, None],
-                  ['Igorosha', 'Prorochuk', None],
-                  ['Jenia', 'Trofimenko', '@jeniaTrofimenko'],
-                  ['Alina', 'Brygas', None],
-                  ['Eugen', None, None]]
+    group_list = [
+        {'First name': 'Alexandr', 'Last name': 'Klimach', 'Telegram tag': '@klimach'},
+        {'First name': 'Alexander', 'Last name': 'Kozhokar', 'Telegram tag': '@hey_alex'},
+        {'First name': 'Viktor', 'Last name': 'B.', 'Telegram tag': '@sancheezzz'},
+        {'First name': 'Anton', 'Last name': 'Ivanov', 'Telegram tag': None},
+        {'First name': 'Roman', 'Last name': 'Rodomansky', 'Telegram tag': '@romanrodomansky'},
+        {'First name': 'Pavlo', 'Last name': 'Kandiak', 'Telegram tag': '@kandiak_ps'},
+        {'First name': 'Eugene', 'Last name': 'Zabolotny', 'Telegram tag': '@eugenezabolotny'},
+        {'First name': 'Дмитро', 'Last name': 'Bragarnik', 'Telegram tag': '@Sarbai'},
+        {'First name': 'Eugene', 'Last name': 'Semanyshyn', 'Telegram tag': None},
+        {'First name': 'Anton', 'Last name': 'Dotsenko', 'Telegram tag': None},
+        {'First name': 'Ruslan', 'Last name': 'Karelov', 'Telegram tag': None},
+        {'First name': 'Ostap', 'Last name': 'Rodomansky', 'Telegram tag': '@ostap_rodomansky'},
+        {'First name': 'Igor', 'Last name': 'Tagintsev', 'Telegram tag': '@Ingvar1390'},
+        {'First name': 'Maxim', 'Last name': 'Poleshko', 'Telegram tag': '@Maxsim_P'},
+        {'First name': 'Misha', 'Last name': 'Antonkin', 'Telegram tag': '@Cosmander'},
+        {'First name': 'Андрій', 'Last name': 'Станішевський', 'Telegram tag': '@xlibchyk'},
+        {'First name': 'Olga', 'Last name': 'Tregub', 'Telegram tag': None},
+        {'First name': 'Roman', 'Last name': 'Zhovna', 'Telegram tag': None},
+        {'First name': 'Yevheniia', 'Last name': 'Kyryliuk', 'Telegram tag': '@EvgeniaCURSOR'},
+        {'First name': 'Roman', 'Last name': 'Buryy', 'Telegram tag': None},
+        {'First name': 'Almost wise', 'Last name': 'Dragon', 'Telegram tag': '@AlmostWise'},
+        {'First name': 'Natali', 'Last name': 'Maslova', 'Telegram tag': None},
+        {'First name': 'Andrii', 'Last name': 'Homeniuk', 'Telegram tag': '@AlmostWise'},
+        {'First name': 'Arthur', 'Last name': 'Veres', 'Telegram tag': '@Artooi'},
+        {'First name': 'Dmytro', 'Last name': 'Melnyk', 'Telegram tag': '@Ekut_v'},
+        {'First name': 'Albert', 'Last name': 'Li', 'Telegram tag': None},
+        {'First name': 'Bohdan', 'Last name': 'Novakivskyy', 'Telegram tag': '@AlmostWise'},
+        {'First name': 'Igor', 'Last name': None, 'Telegram tag': '@siriusnlo'},
+        {'First name': 'Anna', 'Last name': None, 'Telegram tag': None},
+        {'First name': 'Igorosha', 'Last name': 'Prorochuk', 'Telegram tag': None},
+        {'First name': 'Jenia', 'Last name': 'Trofimenko', 'Telegram tag': '@jeniaTrofimenko'},
+        {'First name': 'Alina', 'Last name': 'Brygas', 'Telegram tag': None},
+        {'First name': 'Eugen', 'Last name': None, 'Telegram tag': None}]
 
     create_csv_file(group_list, 'study_group_batman')
