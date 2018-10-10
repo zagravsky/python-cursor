f032fb1d715924eea967c6a54f4503d15963d49e
Klimach <klimach95@gmail.com>
2018-10-10 17:47:24 +0300

    hw 6 solved (add sorted output list of employee)

--- week3/lesson2-oop-in-python/homework/AlexandrKlimach/Task3.py
+++ week3/lesson2-oop-in-python/homework/AlexandrKlimach/Task3.py
@@ -10,7 +10,8 @@ class Company:
             print('Sorry. Little experience')
 
     def __str__(self):
-        return '\n'.join([str(dev) for dev in self.developer_list])
+        sorted_list = sorted(self.developer_list, key=lambda dev: dev.years_experience)
+        return '\n'.join([str(dev) for dev in sorted_list])
 
     def __len__(self):
         return len(self.developer_list)
@@ -29,14 +30,26 @@ if __name__ == "__main__":
     comp = Company('Cursor')
     pDev = PythonDeveloper('Alexandr', 9)
     jDev = JavaDeveloper('Konstantin', 2)
-    rDev = RubyDeveloper('Boris', 8)
+    rDev = RubyDeveloper('Boris', 1)
+    pDev_1 = PythonDeveloper('Grigoriy', 7)
+    jDev_2 = JavaDeveloper('Banjamin', 5)
+    rDev_3 = RubyDeveloper('Denis', 3)
+    pDev_4 = PythonDeveloper('Fedor', 11)
+    jDev_5 = JavaDeveloper('Bogdan', 4)
+    rDev_6 = RubyDeveloper('Kolya', 2)
 
     comp + pDev
     comp + jDev
     comp + rDev
+    comp + pDev_1
+    comp + jDev_2
+    comp + rDev_3
+    comp + pDev_4
+    comp + jDev_5
+    comp + rDev_6
 
     print(comp)
 
-    comp.fire_an_employee('Boris')
+    comp.fire_an_employee('Fedor')
 
     print(comp)
