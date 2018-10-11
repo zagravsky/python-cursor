771c4d3d04fe5ff0d81f82d9c8918b4cc6674666
Apestin-Ilya <43470739+Apestin-Ilya@users.noreply.github.com>
2018-10-11 18:01:52 +0300

    Modified Task_3 and Task_7 after Roman Tsiupka comments

--- week2/lesson2-intro-to-functional/homework/IlyaApestin/Task_3.py
+++ week2/lesson2-intro-to-functional/homework/IlyaApestin/Task_3.py
@@ -2,7 +2,7 @@ from Task_1 import members
 
 print(members)
 
-members = list(map(lambda a: {'age': a['age'],
+members = list(map(lambda a: {'age': a['age'] < 200,
                               'name': a['name'],
                               'load': a['age']/2},
                    members))
--- week2/lesson2-intro-to-functional/homework/IlyaApestin/Task_7.py
+++ week2/lesson2-intro-to-functional/homework/IlyaApestin/Task_7.py
@@ -1,4 +1,6 @@
 def arab_to_rom(number):
+    if number < 0 or number > 4000:
+        return "Number out of range"
     roman_arab_dict = [
         ("M", 1000), ("CM", 900), ("D", 500), ("CD", 400),
         ("C", 100), ("XC", 90), ("L", 50), ("XL", 40),
