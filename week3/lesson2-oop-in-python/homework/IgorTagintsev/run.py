e27e7fe03ab5738c777fae16198e9c9d60c034ad
Igor Tagintsev <t.ingvarr@gmail.com>
2018-10-11 23:11:43 +0300

    week_3_lesson_2_igor_tagintsev fixed bugs in run.py

--- week3/lesson2-oop-in-python/homework/IgorTagintsev/run.py
+++ week3/lesson2-oop-in-python/homework/IgorTagintsev/run.py
@@ -1,6 +1,6 @@
-from utils.task_1 import *
+from utils.task_1 import Developer, PythonDeveloper, JavaDeveloper, RubyDeveloper
 from utils.task_2 import *
-from utils.task_3 import *
+from utils.task_3 import it_company
 
 
 #Task_1
@@ -32,3 +32,25 @@ if __name__ == "__main__":
 
 #Task_3
 print('\n-------------------------Task_3-------------------------')
+if __name__ == "__main__":
+    emp_1 = PythonDeveloper(13, 'Igor Tagintsev')
+    emp_2 = PythonDeveloper(3, 'Adolf Gitler')
+    emp_3 = JavaDeveloper(1, 'Donald Tramp')
+    emp_4 = RubyDeveloper(13, 'Angela Mercel')
+    emp_5 = Developer(1, 'Ivan Ivanov')
+    emp_6 = JavaDeveloper(7, 'Bruse Lee')
+    itcompany = it_company()
+
+    itcompany + emp_1
+    itcompany + emp_2
+    itcompany + emp_3
+    itcompany + emp_4
+    itcompany + emp_5
+    itcompany + emp_6
+    print(itcompany)
+
+    itcompany.firing('Angela Mercel')
+    print(itcompany)
+
+    itcompany.firing('Angela Mercel')
+    print(itcompany)
