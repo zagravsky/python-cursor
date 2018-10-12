2a370da4fdf4039c4821c6aae6599f9e0a46a898
silver-v <seliverstov91@ukr.net>
2018-10-12 17:00:11 +0300

    200 add+

--- week2/lesson2-intro-to-functional/homework/SeliverstovOleksandr/utils/task3.py
+++ week2/lesson2-intro-to-functional/homework/SeliverstovOleksandr/utils/task3.py
@@ -1,3 +1,3 @@
 def load_add(members: list):
 
-    return list(map(lambda member: {'age': member['age'], 'name': member['name'], 'load': member['age']/2}, members))
+    return list(map(lambda member: {'age': member['age'], 'name': member['name'], 'load': member['age']/2} if member['age']<= 200 else '', members))
