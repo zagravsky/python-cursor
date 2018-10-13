a0435ed77c33b4f33622da300fa2032f3b235d83
Anastasiia Kostenko <kostenko.nastasiia@gmail.com>
2018-10-13 22:53:37 +0300

    some change was added to the task_2.py

--- week2/lesson2-intro-to-functional/homework/AnastasiiaKostenko/task_2.py
+++ week2/lesson2-intro-to-functional/homework/AnastasiiaKostenko/task_2.py
@@ -3,7 +3,7 @@ from task_1 import members
 def upper_name_in_dict(members:list):
     for name in members:
         name['name'] = name['name'].upper()
-    return members)
+    return members
 
 print(upper_name_in_dict(members))
 
