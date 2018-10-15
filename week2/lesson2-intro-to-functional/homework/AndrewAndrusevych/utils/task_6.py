63093aaca0e7b919a4018c1637db7d0d90ac5ece
Bozon Higgsa <bozonmarketolog@gmail.com>
2018-10-15 10:16:32 +0300

    fix task_6

--- week2/lesson2-intro-to-functional/homework/AndrewAndrusevych/utils/task_6.py
+++ week2/lesson2-intro-to-functional/homework/AndrewAndrusevych/utils/task_6.py
@@ -1,3 +1,3 @@
 def sort_members_by_len_name(members_list: list):
-    members_list.sort(key=lambda x: x['age'])
-    return sorted(members_list, key=lambda x: len(x['name']))
+    return sorted(members_list, key=lambda k: (len(k['name']), k['age']))
+
