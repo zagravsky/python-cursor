a9dbcc3a08e1b0d0bc2ff0596dadc81ab302c078
Bozon Higgsa <bozonmarketolog@gmail.com>
2018-10-15 11:17:55 +0300

    fix task_5

--- week2/lesson2-intro-to-functional/homework/AndrewAndrusevych/utils/task_5.py
+++ week2/lesson2-intro-to-functional/homework/AndrewAndrusevych/utils/task_5.py
@@ -1,12 +1,3 @@
-def get_summery_age_youngest_and_oldest_member(members_list: list):
-    return __get_summery_age(members_list),__get_youngest_member(members_list),__get_oldest_member(members_list)
-def __get_summery_age(members_list:list):
-    result = 0
-    for member in members_list:
-       result += member['age']
-    return result
-def __get_youngest_member(members_list:list):
-    members_list.sort(key = lambda x:x['age'])
-    return members_list[0]
-def __get_oldest_member(members_list:list):
-    return members_list[len(members_list)-1]
+def get_summery_age_youngest_and_oldest_member(members: list):
+    return sum([member['age'] for member in members]), min(members, key=lambda x: x['age']), max(members, key=lambda x: x['age'])
+
