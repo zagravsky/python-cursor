f993b3c4f111db711232b8f7b57ebebe9b5c4a55
Maxim-Poleshko <poleshkomaxim@gmail.com>
2018-10-17 14:45:17 +0300

    week_3_lesson_5_maxim_poleshko changed the solution

--- week3/lesson1-Python-Standart-Library-HTTP/homework/MaximPoleshko/run.py
+++ week3/lesson1-Python-Standart-Library-HTTP/homework/MaximPoleshko/run.py
@@ -1,10 +1,11 @@
 from utils.task1 import *
 from utils.task2 import *
 from utils.task3 import *
+from utils.members import *
 
 make_dirs(os.getcwd() + "/first/second/third/fourth")
 dir_tree_func(path)
 
-study_group()
+study_group(list)
 
 save_img()
