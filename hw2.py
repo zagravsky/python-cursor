b3558eb4957b2afb4b7cf224a31a41174b59574c
body773 <body.nv@gmail.com>
2018-10-17 10:24:00 +0300

    fix bugs and etc.

--- hw2.py
+++ hw2.py
@@ -1,22 +1,20 @@
-def changenum ():
-	mylist = []
+def changenum():
+	my_list = []
 	for i in range(1,31):
-		b = list(map(int,str(i)))
-		if len(list(b))==1:
+		list_part = list(map(int,str(i)))
+		item_part = str(i)
+		if len(list(list_part))==1:
 			if i%3==0:
-				mylist.append("Buzz")
+				item_part = "Buzz"
 			elif i%5==0:
-				mylist.append("Fuzz")
-			else:
-				mylist.append(i)
+				item_part = "Fuzz"
 		else:
 			if i%3==0 and i%5==0:
-				mylist.append("BuzzFuzz")
+				item_part = "BuzzFuzz"
 			elif i%3==0:
-				mylist.append("Buzz")
+				item_part = "Buzz"
 			elif i%5==0:
-				mylist.append("Fuzz")
-			else:
-				mylist.append(i)
-	return mylist
-print(changenum ())
+				item_part = "Fuzz"
+		my_list.append(item_part)
+	return my_list
+print(changenum())
