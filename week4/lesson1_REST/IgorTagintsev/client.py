6369eab8b59f17e125401e6fb94666d68f66cf08
Igor Tagintsev <t.ingvarr@gmail.com>
2018-10-18 19:42:43 +0300

    week_4_lesson_1_igor_tagintsev solve Task_3

---
+++ week4/lesson1_REST/IgorTagintsev/client.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+
+jsonrpc = {
+    "jsonrpc": "2.0",
+    "method": "getMember",
+    "params": {"name": "Igor"}
+}
+
+try:
+    my_req = requests.post(' http://127.0.0.1:4001/', data=json.dumps(jsonrpc))
+    print(my_req.text)
+    print("200 OK")
+except requests.exceptions.ConnectionError:
+    print("500 Internal Server Error") 
