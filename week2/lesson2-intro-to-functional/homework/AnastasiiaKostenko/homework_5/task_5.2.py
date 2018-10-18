ce08e6d6038c62df57b3f5f0bd18088718cf314a
Anastasiia Kostenko <kostenko.nastasiia@gmail.com>
2018-10-18 22:55:44 +0300

    Second task is done

---
+++ week2/lesson2-intro-to-functional/homework/AnastasiiaKostenko/homework_5/task_5.2.py
@@ -0,0 +1,16 @@
+import csv
+
+with open('group_members.csv', 'r') as csv_file:
+    fieldnames = ['First name', 'Last name', 'Telegram tag']
+    csv_reader = csv.DictReader(csv_file, fieldnames = fieldnames)
+
+    with open('new_member_file.csv', 'w', newline = '') as csv_new_file:
+
+
+        csv_writer = csv.DictWriter(csv_new_file, fieldnames = 
+fieldnames, delimiter='\t')
+
+        csv_writer.writeheader()
+
+        for line in csv_reader:
+            csv_writer.writerow(line)
