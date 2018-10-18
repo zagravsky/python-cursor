530a62a37bd59db7d8a9a02b752e03d17885cc87
Igor Tagintsev <t.ingvarr@gmail.com>
2018-10-18 19:42:12 +0300

    week_4_lesson_1_igor_tagintsev solve Task_1

---
+++ week4/lesson1_REST/IgorTagintsev/task_1.py
@@ -0,0 +1,60 @@
+import json
+import flask
+from flask import Flask
+from flask import request
+
+
+app = Flask(__name__)
+
+MEMBERS = {
+    'Igor': {'age': 28, 'gender': 'male', 'name': 'Igor'},
+}
+
+def check_member(name) -> bool:
+    return name in MEMBERS.keys()
+
+@app.route('/user', methods=['POST'])
+@app.route('/user/<name>', methods=['GET', 'PATCH', 'DELETE'])
+def profile(name: str):
+    result = {}
+
+    if flask.request.method == 'POST':
+        params = json.loads(request.data.decode('utf-8'))
+        MEMBERS[params.get('name')] = params
+        result = {'Status': 'OK', 'message': f'Add new user {params}'}
+
+    if flask.request.method == 'GET':
+        member = MEMBERS.get(name)
+        if member is None:
+            result = {"status": "Fail", "error": f"Could not find member with name {name}"}
+        else:
+            result = {"status": "OK", "message": f"We find your user {member}"}
+
+    if flask.request.method == 'PATCH':
+        member = MEMBERS.get(name)
+        if member is None:
+            result = {"status": "Fail", "message": "I dont know about such user. Sorry"}
+        else:
+            params = json.loads(request.data.decode('utf-8'))
+            MEMBERS[name].update(params)
+            result = {"status": "Ok", "message": f"This is your new member {MEMBERS.get(name)}"}
+
+    if flask.request.method == 'DELETE':
+        if not check_member(name):
+            result = {"status": "Fail", "message": "I dont know about such user. Sorry"}
+        else:
+            del MEMBERS[name]
+            result = {"status": "Ok", "message": f"We delete your member bro"}
+    return json.dumps(result)
+
+@app.route('/members', methods=['GET'])
+def dump_members():
+    if flask.request.method == 'GET':
+        str_members = json.dumps(MEMBERS)
+        json.dump(str_members, open("MEMBERS.json", "w"))
+        return "MEMBERS.json created"
+
+if __name__ == "__main__":
+    server_settings = json.load(open('server_settings.json', 'r'))
+    app.run(port=server_settings["port"], host=server_settings["host"], debug=server_settings["debug"])
+ 
