0457e1cc00acf9f7a76b5c1f72f935108e152fe5
silver-v <seliverstov91@ukr.net>
2018-10-19 02:51:21 +0300

    made corrections marge conflict

--- week2/lesson2-intro-to-functional/homework/SeliverstovOleksandr/utils/task3.py
+++ week2/lesson2-intro-to-functional/homework/SeliverstovOleksandr/utils/task3.py
@@ -1,8 +1,5 @@
 def load_add(members: list):
-<<<<<<< Updated upstream
 
-    return list(map(lambda member: {'age': member['age'], 'name': member['name'], 'load': member['age']/2} if member['age']<= 200 else '', members))
-=======
     sorted_mem=[]
     for member in members:
         if member['age'] < 200:
@@ -11,5 +8,3 @@ def load_add(members: list):
             pass
     return (sorted_mem)
 
-#    return list(map(lambda member: {'age': member['age'], 'name': member['name'], 'load': member['age']/2} if member['age'] <=200 else '', members))
->>>>>>> Stashed changes
