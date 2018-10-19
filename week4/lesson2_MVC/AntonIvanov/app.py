1a126003c289a30a54306997d6d69b66dee7c94d
Aersum <antooniosivanovs@gmail.com>
2018-10-20 01:14:46 +0300

    Home work completely solved

--- week4/lesson2_MVC/AntonIvanov/app.py
+++ week4/lesson2_MVC/AntonIvanov/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from Developer import Developer
+
+
 app = Flask(__name__)
+dev1 = Developer("Oleg", "Schevchuk", "Java")
+dev2 = Developer("Dmytro", "Tuchik", "C++")
+dev3 = Developer("Alex", "Lutenko", "Python")
+dev4 = Developer("Anton", "Ivanov", "Python")
+dev_pack = [dev1, dev2, dev3, dev4]
 
 
 @app.route('/')
@@ -8,3 +15,18 @@ def developer_controller():
     dev = Developer("Anton", "Ivanov", "Python")
     return dev()
     # return 'Hello, World!'
+
+
+@app.route('/remove_developer/<int:index>')
+def remove_developer(index: int):
+    try:
+        dev_pack.pop(index)
+    except IndexError:
+        return f"dev_pack has no index {index}"
+    if dev_pack:
+        resp_string = ''
+        for i in dev_pack:
+            resp_string += '<p>' + i() + '</p>'
+        return resp_string
+    else:
+        return "Developer list is empty"
