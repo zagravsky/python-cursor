aad6e2b1d04ff8a46bea5e0b1e4cd19086256174
Anastasiia Kostenko <kostenko.nastasiia@gmail.com>
2018-10-19 23:56:37 +0300

    Some changes were made

--- week3/task_3.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def download_image(url: str):
-    with open('fff.png', 'bw') as image:
-        response = requests.get(url)
-        image.write(response.content)
-
-download_image('https://dummyimage.com/600x400/000/fff')
