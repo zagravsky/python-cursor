907c4b51504c683bbf45c7ce353b938cb8edb793
Ruslan Karelov <ruslan.karelov@gmail.com>
2018-10-20 17:54:22 +0300

    Fixed return in app_task4.py

--- week4/lesson2-web-app-architecture-MVC/homework/RuslanKarelov/RuslanKarelov/app_task4.py
+++ week4/lesson2-web-app-architecture-MVC/homework/RuslanKarelov/RuslanKarelov/app_task4.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def developer_controler():
     dev1 = Developer("Reslan", "Karelov", "Python")
-    return f"{dev1.first_name} {dev1.last_name} - {dev1.programing_language}"
+    return f"{dev1}"
 
 
