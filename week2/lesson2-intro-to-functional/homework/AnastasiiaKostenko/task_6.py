0ea029d0097514127051a15e4ceedd42c9cb359d
Anastasiia Kostenko <kostenko.nastasiia@gmail.com>
2018-10-21 22:24:49 +0300

    Correction of mistakes in the tasks

--- week2/lesson2-intro-to-functional/homework/AnastasiiaKostenko/task_6.py
+++ week2/lesson2-intro-to-functional/homework/AnastasiiaKostenko/task_6.py
@@ -1,8 +1,7 @@
 from task_1 import members
 
 def sorted_members(members:list):
-    sorted_members_list = sorted(members, key=lambda x: (len(x['name']), 
-x['age']))
+    sorted_members_list = sorted(members, key=lambda x: (len(x['name']), x['age']))
     return sorted_members_list
 
 print(sorted_members(members))
