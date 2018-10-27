29b5879639e5fba524edef0812e9851f9d11db96
Aersum <antooniosivanovs@gmail.com>
2018-10-27 05:19:45 +0300

    Task1 solveed

--- Week5/Lesson9_Flask/AntonIvanov/app_fabrik/__init__.py
+++ Week5/Lesson9_Flask/AntonIvanov/app_fabrik/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from .api import factory_api, test_api, home_api
+from .api import factory_api, test_api, home_api ,bike_api
 from .config import runtime_config
 
 def run_app():
     app = Flask(__name__)
     app.config.from_object(runtime_config())
     app.register_blueprint(factory_api)
+    app.register_blueprint(bike_api)
     app.register_blueprint(test_api)
     app.register_blueprint(home_api)
     return app
--- Week5/Lesson9_Flask/AntonIvanov/app_fabrik/api.py
+++ Week5/Lesson9_Flask/AntonIvanov/app_fabrik/api.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, request, jsonify
 from flask.views import MethodView
-
 from flask import current_app
 from .db import BIKES
+import json
+
 
 class FabrikApiView(MethodView):
     def get(self):
@@ -18,8 +19,37 @@ class HomeView(MethodView):
     def get(self):
         return render_template("home.html",data=BIKES)
 
+class BikesView(MethodView):
+    def get(self, name=None):
+        if name is None:
+            return jsonify(BIKES)
+        elif name in [elem['name'] for elem in BIKES]:
+            for i, elem in enumerate(BIKES):
+                if elem['name'] == name:
+                    return jsonify(BIKES[i])
+        else:
+            return jsonify({"status": "Fail", "message": "I don't know about such bike. Sorry"})
+
+    def post(self):
+        params = json.loads(request.data.decode('utf-8'))
+        BIKES.append(params)
+        return jsonify({"status": "OK", "message": f"Add new bikes {params}"})
+
+    def delete(self, name: str):
+        if name in [elem['name'] for elem in BIKES]:
+            for i, elem in enumerate(BIKES):
+                if elem['name'] == name:
+                    return jsonify(BIKES.pop(i))
+        else:
+            return jsonify({"status": "Fail", "message": "I don't know about such bike. Sorry"})
+
+
 factory_api = Blueprint('factory_api', __name__, static_folder='static', template_folder='templates')
 factory_api.add_url_rule('/factory', view_func=FabrikApiView.as_view('factory_api'))
+
+bike_api = Blueprint('bikes_api', __name__)
+bike_api.add_url_rule('/bike', view_func=BikesView.as_view('bikes_api'))
+bike_api.add_url_rule('/bike/<string:name>', view_func=BikesView.as_view('bike_api'))
 
 test_api = Blueprint('test_api', __name__, static_folder='static', template_folder='templates')
 test_api.add_url_rule('/test', view_func=TestView.as_view('test_api'))
