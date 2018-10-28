66324761ca671eec552c2ad84cb64fca503cce9c
Daria_Homenko <darishahomenkogmail.comgit commit -m First>
2018-10-29 00:47:35 +0300

    week5_lesson1_Daria_Homenko Added db

---
+++ Week5/Lesson9_Flask_Daria_Homenko/db.py
@@ -0,0 +1,17 @@
+news_db = {
+    "news": ["elem", "elem_2", "elem_3"]
+    }
+
+
+flowers_db = {
+    "Voloshka": {"name": "Волошка", "meaning": "Cимвол нестабільності і мінливості",
+                 "image": "https://pustunchik.ua/uploads/school/c0a61bed3fe005a8dc95f74584f93aab.jpg"},
+    "Gvozduka": {"name": "Гвоздика", "meaning":"Символ чистоти і любові",
+                 "image": "https://pustunchik.ua/uploads/school/8bbafe28e65376703ee0d83d203973ae.jpg"},
+    "Gladiolus": {"name": "Гладіолус", "meaning":"Символ перемоги, благородства",
+                  "image": "https://pustunchik.ua/uploads/school/4b176e45e01687349ba291b68d664a04.jpg"},
+    "Jasmine": {"name": "Жасмин", "meaning":"Cимвол привітання, симпатії і приємних спогадів",
+                "image": "https://pustunchik.ua/uploads/school/c3d6c8fd3baac77373d4434ab0c99ea5.jpg"}
+    }
+
+user_db = {"Daria":{"name":"daria", "age":23, "password": 123, "e-mail": "djfkd@s"} }
