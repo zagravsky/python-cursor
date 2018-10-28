3df32d6c7fb1a339eeb84386e3dfb77af77e3144
Daria_Homenko <darishahomenkogmail.comgit commit -m First>
2018-10-29 00:47:19 +0300

    week5_lesson1_Daria_Homenko Added config file

---
+++ Week5/Lesson9_Flask_Daria_Homenko/config.py
@@ -0,0 +1,26 @@
+import os
+
+
+class Config:
+    TEST_VARIABLE = "Config"
+    SECRET_KEY = 'b\xaa;\x0b\x12\x8a\xa1V+\x16\xc5'
+
+
+class DevConfig(Config):
+    TEST_VARIABLE = "Dev Config"
+    SECRET_KEY = os.urandom(16)
+
+
+class TestConfig(Config):
+    TEST_VARIABLE = "Test Config"
+    SECRET_KEY = 'b\xaa;\x0b\x12'
+
+
+def runtime_config():
+    env = os.environ.get("ENV")
+    if env == "DEV":
+        return DevConfig
+    if env == "TEST":
+        return TestConfig
+    else:
+        return Config
