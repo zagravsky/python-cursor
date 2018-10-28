5c93cbb084c7f1b8d7e6e7c96a2c461bed09b171
Zhovna Roman <zhovna.roman@gmail.com>
2018-10-28 20:25:53 +0200

    [week4|lesson2] fixed bugs

--- week4/lesson2-web-app-arch/homework/RomanZhovna/app.py
+++ week4/lesson2-web-app-arch/homework/RomanZhovna/app.py
@@ -12,11 +12,13 @@ class Developer:
         return f"{self.first_name} {self.last_name} - {self.language}"
 
 
-developers = [
-                {"first_name": "Student1", "last_name": "Last_name1", "language": "C"},
-                {"first_name": "Student2", "last_name": "Last_name2", "language": "Python"},
-                {"first_name": "Student3", "last_name": "Last_name3", "language": "Java"},
-             ]
+developers = []
+dev1 = Developer('Student1', 'Last_name1', 'C')
+dev2 = Developer('Student2', 'Last_name2', 'Python')
+dev3 = Developer('Student3', 'Last_name3', 'Java')
+developers.append(dev1)
+developers.append(dev2)
+developers.append(dev3)
 
 
 @app.route('/')
@@ -31,7 +33,10 @@ def remove_developer():
         return f"List of developers is empty"
     else:
         del developers[0]
-        output = ''
-        for dev in developers:
-            output += f"{dev['first_name']} {dev['last_name']} - {dev['language']}\n"
-        return output.replace('\n', '<br />')
+        if not developers:
+            return f"List of developers is empty"
+        else:
+            output = ''
+            for dev in developers:
+                output += f"{dev.first_name} {dev.last_name} - {dev.language}\n"
+            return output.replace('\n', '<br />')
