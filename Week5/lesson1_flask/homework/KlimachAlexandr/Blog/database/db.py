2ef73d1cde5d82e46279c43a9d3af6c6988a8e20
klimach <klimach95@gmail.com>
2018-10-30 03:31:23 +0200

    correct v2

--- Week5/lesson1_flask/homework/KlimachAlexandr/Blog/database/db.py
+++ Week5/lesson1_flask/homework/KlimachAlexandr/Blog/database/db.py
@@ -2,22 +2,22 @@ import json
 
 
 def get_db():
-    with open('Blog/database/db.json', 'r') as db:
+    with open('database/db.json', 'r') as db:
         data = json.load(db)
     return data
 
 
 def write_db(data: dict):
-    with open('Blog/database/db.json', 'w') as db:
+    with open('database/db.json', 'w') as db:
         json.dump(data, db)
 
 
 def get_user_db():
-    with open('Blog/database/db_user.json', 'r') as db:
+    with open('database/db_user.json', 'r') as db:
         data = json.load(db)
     return data
 
 
 def write_user_db(data: dict):
-    with open('Blog/database/db_user.json', 'w') as db:
+    with open('database/db_user.json', 'w') as db:
         json.dump(data, db)
