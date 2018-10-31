b69255c76c773713bfc6ab74054c868ab20b5f4c
Maxim-Poleshko <poleshkomaxim@gmail.com>
2018-10-31 19:34:32 +0200

    fix task1

---
+++ week3/lesson1-Python-Standart-Library-HTTP/homework/MaximPoleshko/run.py
@@ -0,0 +1,12 @@
+from utils.task1 import *
+from utils.task2 import *
+from utils.task3 import *
+from utils.members import *
+
+path = "/home/max/forcoding/"
+make_dirs(path)
+dir_tree_func(path)
+
+study_group(list)
+
+save_img()
