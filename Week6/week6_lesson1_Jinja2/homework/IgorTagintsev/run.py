9976460eeb6f8d9fae83cf0c52045b9066cfc931
Igor Tagintsev <t.ingvarr@gmail.com>
2018-11-01 00:13:12 +0200

    week_6_lesson_1_igor_tagintsev solve Jinja2

---
+++ Week6/week6_lesson1_Jinja2/homework/IgorTagintsev/run.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Task_1', time=datetime.now())
+
+@app.route('/newpage')
+def newpage():
+    return render_template('newpage.html', title='Task_2')
+
+if __name__ == '__main__':
+    app.run(debug=True) 
