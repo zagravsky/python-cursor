7a752e7943263bcdda683d5d53577311fd4c36bb
Igor Tagintsev <t.ingvarr@gmail.com>
2018-11-01 22:19:45 +0200

    week_4_lesson_2_igor_tagintsev fixed bugs in HW4_L2

--- week4/lesson2_Web app architecture/IgorTagintsev/app.py
+++ week4/lesson2_Web app architecture/IgorTagintsev/app.py
@@ -2,11 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-dev_list = [
-    {'first_name': 'Marshall', 'last_name': 'Mathers', 'programming_language': 'PHP'},
-    {'first_name': 'Margaret', 'last_name': 'Thatcher', 'programming_language': 'Java'},
-    {'first_name': 'Anthony', 'last_name': 'Hopkins', '': 'C++'},
-]
+dev_list = []
 
 class Developer():
     def __init__(self, first_name, last_name, programming_language):
@@ -15,19 +11,28 @@ class Developer():
         self.programming_language = programming_language
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name} - {self.programming_language}'
+    	return f'{self.first_name} {self.last_name} - {self.programming_language}'
 
+marshall = Developer('Marshall', 'Mathers', 'PHP')
+margaret = Developer('Margaret', 'Thatcher', 'Java')
+anthony = Developer('Anthony', 'Hopkins', 'C++')
+
+dev_list.append(marshall)
+dev_list.append(margaret)
+dev_list.append(anthony)
+
+def output_list():
+	return '<br>'.join([str(d) for d in dev_list])
 
 @app.route('/')
 def developer_controller():
     dev = Developer('Igor', 'Tagintsev', 'Python')
     return str(dev)
 
-
 @app.route('/remove_developer')
 def remove_developers():
-    if len(dev_list) > 1:
-        dev_list.pop()
-        return '<br>'.join([str(f"{x['first_name']} {x['last_name']} - {x['programming_language']}") for x in dev_list])
-    else:
-        return 'List is empty'
+	if not dev_list:
+		return 'List is empty'
+	else:
+		dev_list.pop()
+		return 'List is empty' if not dev_list else output_list()
