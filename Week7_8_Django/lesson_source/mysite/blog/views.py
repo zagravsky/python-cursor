b1727a84755edaee0689678798ce72489f1526fc
Albert Li <albert.li.das@gmail.com>
2018-11-06 20:52:14 +0200

    Lesson 2. In progress

--- Week7_8_Django/lesson_source/mysite/blog/views.py
+++ Week7_8_Django/lesson_source/mysite/blog/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Article
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
@@ -8,5 +8,16 @@ def index(request):
     return render(request, 'index.html', {'articles': articles})
 
 def detail(request, article_id):
-    article = Article.objects.get(id=article_id)
+    article = get_object_or_404(Article, pk=article_id)
     return render(request, 'detail.html', {'article': article})
+
+def add_article(request):
+    if request.POST:
+        Article.objects.create(
+            title=request.POST.get('title'),
+            description=request.POST.get('description'),
+            author=request.POST.get('author')
+        )
+        print('======================')
+        print(request.POST)
+    return render(request, 'add_article.html')
