3c26285e64823203e909c7605849464aac6e822e
Daria_Homenko <darishahomenkogmail.comgit commit -m First>
2018-11-08 23:48:13 +0300

    Week6_lesson2_Daria_Homenko Added app

---
+++ Week6/week6_lesson12_Daria_Homenko/my_app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+from flask_bcrypt import Bcrypt
+from config import runtime_config
+from api.view import pages_view
+from db.db_creation import create_db
+from db.relationship import one_to_many
+from api.user_api import user_api
+from api.flower_api import flower_api
+
+from db.db_app import db, ma
+
+
+def run_app():
+    app = Flask(__name__)
+
+    db.init_app(app)
+    ma.init_app(app)
+    app.config.from_object(runtime_config())
+    app.register_blueprint(pages_view)
+    app.secret_key = runtime_config().SECRET_KEY
+    app.register_blueprint(create_db)
+    app.register_blueprint(one_to_many)
+    app.register_blueprint(user_api)
+    app.register_blueprint(flower_api)
+    Bcrypt(app)
+    return app
