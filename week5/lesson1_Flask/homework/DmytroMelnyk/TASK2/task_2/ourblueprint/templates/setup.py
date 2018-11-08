39344f0a517c46a3667883789f938f49a96515d6
Melnyk Dmytro <ekut2104@gmail.com>
2018-11-08 08:30:06 +0200

    some changes in setup.py

---
+++ week5/lesson1_Flask/homework/DmytroMelnyk/TASK2/task_2/ourblueprint/templates/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python-cursor',
+    version='3.2',
+    packages=[''],
+    package_dir={'': 'week6/lesson2_Database_ORM/homework/DmytroMelnyk/Car_application'},
+    url='',
+    license='',
+    author='Melnyk Dmytro',
+    author_email='ekut2104@gmail.com',
+    description='flask app - Cars view as blog'
+)
