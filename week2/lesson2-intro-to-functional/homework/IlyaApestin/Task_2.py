8ee1f38edc5c3185a0de1bf75224ef27dd50540a
Apestin-Ilya <ilyon@ukr.net>
2018-11-15 10:13:30 +0200

    Folders structured

--- week2/lesson2-intro-to-functional/homework/IlyaApestin/Task_2.py
+++
@@ -1,5 +0,0 @@
-def upper_name(members):
-    members_upper = list(map(lambda a: {'age': a['age'],
-                                  'name': a['name'].upper()},
-                       members))
-    return members_upper
--- week2/lesson2-intro-to-functional/homework/IlyaApestin/Task_4.py
+++
@@ -1,3 +0,0 @@
-def members_o(members):
-    members_o = list(filter(lambda a: 'o' in a['name'].lower(), members))
-    return members_o
