66462105eb7c899ad786fab59132c597dfe12c2b
denisV <vasilov345@gmail.com>
2018-11-30 19:45:37 +0200

    week10 gang of four add fabric

---
+++ week10/gang_of_four/creational/fabric.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def who_am_i(self):
+        raise NotImplementedError
+
+
+class Circle(Shape):
+    def who_am_i(self):
+        print(f"I am a true circle. Look {self.__class__.__name__}")
+
+
+class Line(Shape):
+    def who_am_i(self):
+        print(f"I am a true Line. Look {self.__class__.__name__}")
+
+
+SHAPES = {
+    "circle": Circle,
+    "line": Line,
+}
+
+
+def create_fig(name):
+    if name in SHAPES:
+        obj = SHAPES.get(name)
+    else:
+        raise AttributeError("No such fig")
+    return obj()
+
+
+if __name__ == "__main__":
+    fig = create_fig("circle")
+    fig.who_am_i()
