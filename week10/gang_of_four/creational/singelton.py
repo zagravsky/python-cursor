6c0e95321183a8f575a270fc9f60a93a558dcd74
denisV <vasilov345@gmail.com>
2018-12-04 19:45:08 +0200

    week10 gang of four last changes

--- week10/gang_of_four/creational/singelton.py
+++ week10/gang_of_four/creational/singelton.py
@@ -1,17 +1,46 @@
 from __future__ import annotations
 
+from typing import List
+
+
+class ConfigPool:
+    __configs: List[Config] = []
+    __max_configs = 5
+
+    def init_configs(self):
+        for _ in range(self.__max_configs):
+            self.__configs.append(Config())
+
+    def get_config(self):
+        for config in self.__configs:
+            if config.state:
+                config.state = 0
+                return config
+        else:
+            raise Exception("No configs are availabe")
+
 
 class Config:
-    __instance: Config = None
+    state = 1
+
+
+class MultitoneConfig:
+    __count = 0
+    __max_count = 3
 
     def __new__(cls, *args, **kwargs):
         print("Method __new__ is calling")
-        if Config.__instance is None:
-            print("We have no inst yet")
-            cls.__instance = super().__new__(cls)
+        # if Config.__instance is None:
+        #     print("We have no inst yet")
+        #     cls.__instance = super().__new__(cls)
+        # else:
+        #     print("Return old one")
+        # return cls.__instance
+        if cls.__count < cls.__max_count:
+            cls.__count += 1
+            return super().__new__(cls)
         else:
-            print("Return old one")
-        return cls.__instance
+            raise Exception("Too much objects")
 
     def __init__(self):
         print("Method init is calling")
@@ -23,6 +52,8 @@ class OrdinaryConfig:
 if __name__ == "__main__":
     first_conf = Config()
     second_conf = Config()
+    third_conf = Config()
+    fourth_conf = Config()
     # ord_conf_first = OrdinaryConfig()
     # ord_conf_second = OrdinaryConfig()
     # print(first_conf is second_conf)
