e38bcb7c64dc15d4116ef14871fd2bee1e710162
romanponomaryov <infinitywarden@gmail.com>
2018-12-04 16:26:51 +0200

    Corrections to HW#8 (week_4_lesson_2) as per supports' recommendations

--- app.py
+++ app.py
@@ -1,42 +1,37 @@
 from flask import Flask
-import json
-
 app = Flask(__name__)
+
+
+class Developer:
+    def __init__(self, first_name, last_name, programming_language):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.programming_language = programming_language
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.programming_language}'
 
 
 @app.route('/')
 def developer_controller():
     dev_instance = Developer('roman', 'ponomaryov', 'python3')
-    return dev_instance.get_info()
+    return str(dev_instance)
 
 
-dev_array = [['Guido', 'van Rossum', 'Python'],
-            ['Dennis', 'Ritchie', 'C'],
-            ['James', 'Gosling', 'Java'],
-            ['Yukihiro', 'Matsumoto', 'Ruby']]
-
-
-class Developer:
-    def __init__(self, first_name, last_name, programming_language):
-        self.first_name = str(first_name)
-        self.last_name = str(last_name)
-        self.programming_language = str(programming_language)
-
-    def get_info(self):
-        return f'{self.first_name} {self.last_name} - {self.programming_language}'
+dev_array = [
+            Developer('Guido', 'van Rossum', 'Python'),
+            Developer('Dennis', 'Ritchie', 'C'),
+            Developer('James', 'Gosling', 'Java'),
+            Developer('Yukihiro', 'Matsumoto', 'Ruby')
+            ]
 
 
 @app.route('/remove_developer')
 def remove_developer():
-    if len(dev_array) >= 2:
-        dev_array.pop(0)
-        output_string = ''
-        for i in dev_array:
-            dev_instance = Developer(i[0], i[1], i[2])
-            output_string += f'{dev_instance.get_info()} <br>'
-        return json.dumps(output_string).replace('"', '')
-    elif len(dev_array) == 1:
-        dev_array.pop(0)
+    if not dev_array:
         return 'Developers list is empty'
-    else:
-        return 'Developers list is empty'
+    dev_array.pop(0)
+    if dev_array:
+        output_string = '<br>'.join(list(map(lambda dev: str(dev), dev_array)))
+        return output_string.replace('[', '').replace(']', '').replace('\'', '')
+    return 'Last developer was removed'
