d1dabb8533a99161ef63031963917cf7921817f0
denisV <vasilov345@gmail.com>
2018-12-04 20:17:51 +0200

    weeek11 NoSql

--- week11/main.py
+++ week11/main.py
@@ -1,13 +1,18 @@
 import argparse
 
-from redis import Redis
-
 from no_sql.redis.redis_handler import RedisHandler
 
-dbs = {"redis" : RedisHandler}
+dbs = {"redis": RedisHandler}
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Our first script')
+    # parser.add_argument('-st', '--storage', help='Name of storage', required=True)
+    parser.add_argument('-ht', '--host', dest="host")
+    parser.add_argument('-pt', '--port', dest="port")
+    parser.add_argument('-db', '--db', dest="db")
 
-    r = Redis()
-    name = r.get("name")
-    print(name)
+    args = parser.parse_args()
+
+    r = RedisHandler(host=args.host, port=args.port, db=args.db)
+
+    print(r.conn.get("name"))
