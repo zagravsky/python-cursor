1da7993b96165acce8d6ea42bb082b7a2854530f
Maxim-Poleshko <poleshkomaxim@gmail.com>
2018-12-13 00:25:54 +0200

    fix task4

--- week2/lesson2-intro-to-functional/homework/MaximPoleshko/utils/task_4.py
+++ week2/lesson2-intro-to-functional/homework/MaximPoleshko/utils/task_4.py
@@ -1,6 +1,2 @@
-def left_members(memb: list) -> list:
-    without_o = []
-    for m in memb:
-        if not 'o' in m['name'].lower():
-            without_o.append(m)
-    return without_o
+def left_members(members: list):
+    return list(filter(lambda x: "o" in x["name"], members))
