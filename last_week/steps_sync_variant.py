a319d6b2210d6eac1133ac7403dacebe990637b0
denisV <vasilov345@gmail.com>
2018-12-25 19:34:04 +0200

    AsyncPython

--- last_week/steps_sync_variant.py
+++ last_week/steps_sync_variant.py
@@ -2,25 +2,24 @@ import time
 
 
 def left_foot(step_time, distance=10):
-    for dis in range(1, distance):
-        print("Prepare for left step")
-        time.sleep(step_time)
-        # await asyncio.sleep(step_time)
-        print("Left step")
+    # for dis in range(1, distance):
+    print("Prepare for left step")
+    time.sleep(step_time)
+    print("Left step")
 
 
 def right_foot(step_time, distance=10):
-    for dis in range(1, distance):
-        print("Prepare for right step")
-        time.sleep(step_time)
-        # await asyncio.sleep(step_time)
-        print("Right step")
+    # for dis in range(1, distance):
+    print("Prepare for right step")
+    time.sleep(step_time)
+    print("Right step")
 
 
 def main():
-    tasks = (right_foot, left_foot)
-    for task in tasks:
-        task(1, 5)
+    # tasks = (right_foot, left_foot)
+    for step in range(0, 10):
+        left_foot(1)
+        right_foot(1)
 
 
 if __name__ == "__main__":
