c16c4e2285a899f19b10735f1c2a1e26bf65fedb
Melnyk Dmytro <ekut2104@gmail.com>
2018-12-26 16:50:50 +0200

    manage.py with local settings

--- week8/lesson1_Django_Forms_GenericViews_Redirections/homework/DmytroMelnyk/myproj/manage.py
+++ week8/lesson1_Django_Forms_GenericViews_Redirections/homework/DmytroMelnyk/myproj/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproj.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproj.settings_master_local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
